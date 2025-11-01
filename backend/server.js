d0700591090a1d7c7ddf24e7c67bf7b01540216d
Jayasuryaprakashreddy333 <jayasuryaprakashreddykuppiredd@gmail.com>
2025-11-01 00:32:49 +0000

    changed port

--- backend/server.js
+++ backend/server.js
@@ -158,6 +158,7 @@ app.get("/tasks/insights", async (req, res) => {
          
 // app listen on port 3000
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
